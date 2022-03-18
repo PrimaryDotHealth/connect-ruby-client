0a93babb363ca880470be3dab06e36a7077688df
Sam Sohn <sam.sohn28@gmail.com>
2022-03-18 11:31:46 -0700

    add lab report endpoint

--- lib/primary_connect_client/api/default_api.rb
+++ lib/primary_connect_client/api/default_api.rb
@@ -139,6 +139,71 @@ module PrimaryConnectClient
       return data, status_code, headers
     end
 
+    # show lab report
+    # @param order_id [String] Order id
+    # @param result_id [String] Result id
+    # @param [Hash] opts the optional parameters
+    # @return [nil]
+    def get_lab_report(order_id, result_id, opts = {})
+      get_lab_report_with_http_info(order_id, result_id, opts)
+      nil
+    end
+
+    # show lab report
+    # @param order_id [String] Order id
+    # @param result_id [String] Result id
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(nil, Integer, Hash)>] nil, response status code and response headers
+    def get_lab_report_with_http_info(order_id, result_id, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: DefaultApi.get_lab_report ...'
+      end
+      # verify the required parameter 'order_id' is set
+      if @api_client.config.client_side_validation && order_id.nil?
+        fail ArgumentError, "Missing the required parameter 'order_id' when calling DefaultApi.get_lab_report"
+      end
+      # verify the required parameter 'result_id' is set
+      if @api_client.config.client_side_validation && result_id.nil?
+        fail ArgumentError, "Missing the required parameter 'result_id' when calling DefaultApi.get_lab_report"
+      end
+      # resource path
+      local_var_path = '/api/v1/orders/{order_id}/results/{result_id}/lab_report'.sub('{' + 'order_id' + '}', CGI.escape(order_id.to_s)).sub('{' + 'result_id' + '}', CGI.escape(result_id.to_s))
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type]
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || ['bearer_auth']
+
+      new_options = opts.merge(
+        :operation => :"DefaultApi.get_lab_report",
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: DefaultApi#get_lab_report\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
     # show result
     # @param order_id [String] Order id
     # @param id [String] Result id
